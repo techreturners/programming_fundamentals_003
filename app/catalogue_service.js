a8355e884a4c640598cd3659edf31485aed06d06
Harriet Ryder <harriethryder@gmail.com>
2019-11-07 16:03:24 +0000

    Update our function to receive the catalogue as an argument

--- app/catalogue_service.js
+++ app/catalogue_service.js
@@ -1,29 +1,14 @@
-// eslint-disable-next-line no-unused-vars
-const catalogue = [
-  "The Catcher in the Rye by J.D. Salinger (10)",
-  "Dracula by Bram Stoker (0)",
-  "Between the Assassinations by Aravind Adiga (9)",
-  "Wolf Hall by Hilary Mantel (33)",
-  "Bring Up The Bodies by Hilary Mantel (31)",
-  "A Place of Greater Safety by Hilary Mantel (11)",
-  "Giving Up the Ghost by Hilary Mantel (8)",
-  "The Assassination of Margaret Thatcher by Hilary Mantel (43)",
-  "The Yellow Wallpaper by Charlotte Perkins Gilman (12)",
-  "Conversations With Friends by Sally Rooney (1)",
-  "Normal People by Sally Rooney (2)",
-  "Everything I Never Told You by Celeste Ng (6)",
-  "2666 by Robert Bolaño (17)",
-  "By Night In Chile by Robert Bolaño (8)",
-  "A Tale of Two Cities by Charles Dickens (3)",
-  "Oliver Twist by Charles Dickens (7)",
-  "Great Expectations by Charles Dickens (1)",
-  "The Blind Assassin by Margaret Atwood (8)",
-  "Why Be Happy When You Could Be Normal? by Jeanette Winterson (19)",
-  "The Origin of Species by Charles Darwin (50)"
-];
+function countBooksByAuthor(author, booksArray) {
+  author = author.toLowerCase();
+  let total = 0;
+  for (let i = 0; i < booksArray.length; i++) {
+    const book = booksArray[i];
+    if (book.author.toLowerCase().replace("ñ", "n") === author) {
+      total++;
+    }
+  }
 
-function countBooksByAuthor(author) {
-  // Your code here
+  return total;
 }
 
 function checkBookByTitle(title) {
--- test/catalogue_service.test.js
+++ test/catalogue_service.test.js
@@ -2,10 +2,126 @@ const catalogueService = require("../app/catalogue_service");
 
 describe("catalogueService", () => {
   describe("catalogueService.countBooksByAuthor", () => {
+    // eslint-disable-next-line no-unused-vars
+    const catalogue = [
+      {
+        title: "The Catcher in the Rye",
+        author: "J.D. Salinger",
+        quantity: 10
+      },
+      {
+        title: "Dracula",
+        author: "Bram Stoker",
+        quantity: 0
+      },
+      {
+        title: "Between the Assassinations",
+        author: "Aravind Adiga",
+        quantity: 9
+      },
+      {
+        title: "Wolf Hal",
+        author: "Hilary Mantel",
+        quantity: 33
+      },
+      {
+        title: "Bring Up The Bodies",
+        author: "Hilary Mantel",
+        quantity: 31
+      },
+      {
+        title: "A Place of Greater Safety",
+        author: "Hilary Mantel",
+        quantity: 11
+      },
+      {
+        title: "Giving Up the Ghost",
+        author: "Hilary Mantel",
+        quantity: 8
+      },
+      {
+        title: "The Assassination of Margaret Thatcher",
+        author: "Hilary Mantel",
+        quantity: 43
+      },
+      {
+        title: "The Yellow Wallpaper",
+        author: "Charlotte Perkins Gilmán",
+        quantity: 12
+      },
+      {
+        title: "Conversations With Friends",
+        author: "Sally Rooney",
+        quantity: 1
+      },
+      {
+        title: "Normal People",
+        author: "Sally Rooney",
+        quantity: 2
+      },
+      {
+        title: "Everything I Never Told You",
+        author: "Celeste Ng",
+        quantity: 6
+      },
+      {
+        title: "2666",
+        author: "Robert Bolaño",
+        quantity: 17
+      },
+      {
+        title: "By Night In Chile",
+        author: "Robert Bolaño",
+        quantity: 8
+      },
+      {
+        title: "A Tale of Two Cities",
+        author: "Charles Dickens",
+        quantity: 3
+      },
+      {
+        title: "Oliver Twist",
+        author: "Charles Dickens",
+        quantity: 7
+      },
+      {
+        title: "Great Expectations",
+        author: "Charles Dickens",
+        quantity: 1
+      },
+      {
+        title: "The Blind Assassin",
+        author: "Margaret Atwood",
+        quantity: 8
+      },
+      {
+        title: "Why Be Happy When You Could Be Normal?",
+        author: "Jeanette Winterson",
+        quantity: 19
+      },
+      {
+        title: "The Origin of Species",
+        author: "Charles Darwin",
+        quantity: 50
+      }
+    ];
+
     test("returns the total number of books written by the given author", () => {
-      expect(catalogueService.countBooksByAuthor("Hilary Mantel")).toBe(5);
-      expect(catalogueService.countBooksByAuthor("Celeste Ng")).toBe(1);
-      expect(catalogueService.countBooksByAuthor("Charles Dickens")).toBe(3);
+      expect(catalogueService.countBooksByAuthor("Hilary Mantel", catalogue)).toBe(5);
+      expect(catalogueService.countBooksByAuthor("Celeste Ng", catalogue)).toBe(1);
+      expect(catalogueService.countBooksByAuthor("Charles Dickens", catalogue)).toBe(3);
+    });
+
+    test("returns 0 if the book isn't found", () => {
+      expect(catalogueService.countBooksByAuthor("JK Rowling", catalogue)).toBe(0);
+    });
+
+    test("is not case sensitive", () => {
+      expect(catalogueService.countBooksByAuthor("hilary mantel", catalogue)).toBe(5);
+    });
+
+    test("is not sensitive to accented characters", () => {
+      expect(catalogueService.countBooksByAuthor("robert bolano", catalogue)).toBe(2);
     });
   });
 });
